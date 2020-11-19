f7680548b24c20fbbc83a8c1042566bd5c8fc711
DESKTOP-N1D5FAL\Darius <buhaidarius@gmail.com>
2020-11-19 17:12:12 +0200

    Update 3 Elemente cu REST API

---
+++ Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProiectDAW.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
